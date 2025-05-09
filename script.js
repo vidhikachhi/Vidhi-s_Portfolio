32725221b7811ed56743556e321c92b2d4064ee1
Vidhi kachhi <118750257+vidhikachhi@users.noreply.github.com>
2025-05-09 19:17:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+  function sendMail(event) {
+    event.preventDefault(); // Prevent form submission
+
+    let parms = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      subject: document.getElementById("subject").value,
+      message: document.getElementById("message").value,
+    };
+
+    emailjs.send("service_smwd3ux", "template_24am6e5", parms)
+      .then(() => {
+        alert("Your email has been sent");
+      })
+      .catch((error) => {
+        alert("Failed to send email: " + error);
+      });
+  }
